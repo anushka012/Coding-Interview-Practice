9fc95558632ce8f84084fff3b0849e5e9c594cdc
Anushka S <66209295+anushka012@users.noreply.github.com>
2020-11-04 23:34:46 +0530

    Add files via upload

---
+++ MergeSortedArray.py
@@ -0,0 +1,21 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        arr = nums1[:m]
+        i,j = 0,0
+        
+        while (i<m and j<n):
+            if arr[i] <= nums2[j]:
+                nums1[i+j] = arr[i] 
+                i+=1
+                
+            else:
+                nums1[i+j] = nums2[j]
+                j+=1
+                
+        if i==m:
+            nums1[(i+j):] = nums2[j:]
+        if j==n:
+            nums1[(i+j):] = nums1[i:]
